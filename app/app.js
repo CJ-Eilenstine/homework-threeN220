e7218bc5db080ce97c99811194646e963deb50b1
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2023-09-12 11:47:17 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,13 @@
+function calcInt(){
+let num1 = document.getElementById("principle").value;
+let num2 = document.getElementById("interest-rate").value;
+let num3 = document.getElementById("time").value;
+
+
+}
+// results
+
+function changeText() {
+
+document.getElementById("final-results").innerHTML = "With a beginning principle of " + num1;
+}
