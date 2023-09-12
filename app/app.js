029a74f66637589107f9b7f1c70d4817af4d5707
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2023-09-12 15:37:29 -0400

    some stuff
    I'm tired and wanna finish this.

--- app/app.js
+++ app/app.js
@@ -1,13 +1,12 @@
-function calcInt(){
+// results
+function changeText() {
+
 let principal = parseFloat(document.getElementById("principle").value);
 let rate = parseFloat(document.getElementById("interest-rate").value);
 let time = parseFloat(document.getElementById("time").value);
 
+let total = principal * (rate * time + 1);
 
-}
-// results
-
-function changeText() {
-
-document.getElementById("final-results").innerHTML = "With a beginning principle of " + pricipal + " and with a growth rate of " + rate + "% for " + time + " years, your total interest will be $";
+document.getElementById("final-results").innerHTML = "With a beginning principle of " + principal + " and with a growth rate of " + rate + "% for " + time + " years, your total interest will be $" + principle* (++rate * time ) + " with a grand total of $" + total;
+console.log("hello");
 }
